651e9fd4d16f100807d1450cc25c48bb84da1bde
danisalo <your_github_email>
2023-02-20 19:09:26 +0100

    conflicts

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -10,16 +10,16 @@ const User = require('./../models/User.model')
 const { currentUser, isLoggedIn, isLoggedOut, checkRole } = require('../middlewares/route-guard')
 
 // Create Event form render #1
-router.get('/crear-evento-1', /*middleware organizer*/(req, res, next) => { res.render('event/create-event') })
+router.get('/crear-evento-1', /*middleware organizer*/(req, res, next) => { res.render('event/create-event-1') })
 
 // Create Event form handler #1
 router.post('/crear-evento-1', /*middleware organizer*/(req, res, next) => {
     const { searchBook } = req.body
-    const { currentUser_id } = req.session.currentUser
+    // const { currentUser_id } = req.session.currentUser
 
     Event
         .searchBooks(searchBook)
-        .create({ searchBook, owner: currentUser_id })
+        // .create({ searchBook, owner: currentUser_id })
         .then(() => res.redirect(`/cloob/${cloob._id}`))
         .catch(err => next(err))
 })

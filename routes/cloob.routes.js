5b3992fadda5ce70db4f327f8553fbf2ed830067
Ruben Briongos <ruben.briongos@gmail.com>
2023-02-20 16:17:42 +0100

    cloob routes + cloob form

--- routes/cloob.routes.js
+++ routes/cloob.routes.js
@@ -1,14 +1,102 @@
 const express = require('express')
 const router = express.Router()
-
-// Models
-// aqui
+const Cloob = require('./../models/Cloob.model')
 
 const { isLoggedIn, checkRole } = require('../middlewares/route-guard')
 
-// Lista
-router.get("/lista", (req, res, next) => {
-    res.render("cloob/list")
+////////////////////////////////////////////
+
+router.get('/lista', isLoggedIn, (req, res, next) => {
+
+    Cloob
+        .find()
+        .then(cloobs => {
+            res.render('cloob/list', {
+                cloobs: cloobs,
+                isAdmin: req.session.currentUser?.role === 'ADMIN',       // ROLES: contenido renderizado por rol
+                // isEditor: req.session.currentUser?.role === 'EDITOR',
+            })
+        })
+        .catch(err => next(err))
 })
+
+
+router.get('/mis-cloobs', isLoggedIn, (req, res, next) => {
+
+    Cloob
+        .find({ owner: req.session.currentUser._id })             // CONTENIDO PROPIETARIO: filtrar por dueño/a
+        .then(cloobs => res.render('cloobs/list', { cloobs }))
+        .catch(err => next(err))
+})
+
+
+// Create form render
+router.get('/crear', isLoggedIn, (req, res) => {
+    res.render('cloob/new-cloob-form')
+})
+
+
+// Create form handling
+router.post('/crear', isLoggedIn, (req, res, next) => {
+
+    const { name, description, maxParticipants, cover } = req.body
+    const { _id } = req.session.currentUser                   // CONTENIDO PROPIETARIO: almacenar ID en creación
+
+    Cloob
+        .create({ name, description, maxParticipants, cover, owner: _id })
+        .then(cloob => res.redirect('/'))
+        .catch(err => next(err))
+})
+
+
+// Render cloob details
+router.get('/cloob/:cloob_id', (req, res, next) => {
+
+    const { cloob_id } = req.params
+
+    Cloob
+        .findById(cloob_id)
+        .then(cloob => res.render(`cloob/${cloob.name}`, cloob))
+        .catch(err => next(err))
+})
+
+
+
+// Edit form render
+router.get('/editar/:cloob_id', isLoggedIn, checkRole('ADMIN', 'HOST'), (req, res, next) => {    // ROLES: acceso por rol
+
+    const { cloob_id } = req.params
+
+    Cloob
+        .findById(cloob_id)
+        .then(cloob => res.render('cloob/edit-cloob-form', cloob))
+        .catch(err => next(err))
+})
+
+
+// Edit form handler
+router.post('/editar', isLoggedIn, checkRole('ADMIN', 'EDITOR'), (req, res) => {         // ROLES: acceso por rol
+
+    const { name, description, maxParticipants, cover, cloob_id } = req.body
+
+    Cloob
+        .findByIdAndUpdate(cloob_id, { name, description, maxParticipants, cover })
+        .then(cloob => res.redirect(`/cloob/${cloob._id}`))
+        .catch(err => console.log(err))
+})
+
+
+
+// Delete cloob
+router.post('/eliminar/:cloob_id', isLoggedIn, checkRole('ADMIN'), (req, res, next) => {          // ROLES: acceso por rol
+
+    const { cloob_id } = req.params
+
+    Cloob
+        .findByIdAndDelete(cloob_id)
+        .then(() => res.redirect('/cloob/lista'))
+        .catch(err => next(err))
+})
+
 
 module.exports = router

47e820d8b3da461f025d2b97fe479f2932833814
Ruben Briongos <ruben.briongos@gmail.com>
2023-02-22 10:54:14 +0100

    index route css view

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,12 +1,32 @@
 const express = require('express')
 const router = express.Router()
 
-// Middlewares
+const Cloob = require('./../models/Cloob.model')
+
 const { currentUser, isLoggedIn, isLoggedOut, checkRole } = require('../middlewares/route-guard')
 
-// Index
+
 router.get("/", (req, res, next) => {
-  res.render("index")
+
+  Cloob
+    .find()
+    .populate({
+      path: 'host',
+      select: '_id firstName lastName'
+    })
+    .sort({
+      maxParticipants: -1,
+      createdAt: -1
+    })
+    .limit(4)
+    .then(cloobs => res.render('index', {
+      cloobs,
+      isAdmin: req.session.currentUser?.role === 'ADMIN'
+    }))
+    .catch(err => next(err))
+
 })
+
+
 
 module.exports = router
